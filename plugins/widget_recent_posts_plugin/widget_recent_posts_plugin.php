69c54fec2cda038e8ade0538e66d908a7991d12a
rnJambre <zarko_stn@yahoo.com>
2018-12-08 13:02:48 +0100

    imenovan i kreiran widget i registrovan u apearance -> widgets

--- plugins/widget_recent_posts_plugin/widget_recent_posts_plugin.php
+++ plugins/widget_recent_posts_plugin/widget_recent_posts_plugin.php
@@ -9,3 +9,25 @@ Author URI: http://www.arcs.org.rs
 */
 
 
+class Widget_Recent_Posts_Plugin extends WP_Widget {
+    
+    function __construct() {
+		parent::__construct(
+
+			// base ID of the widget
+			'widget_recent_posts_plugin',
+
+			// name of the widget
+			__( 'Widget Recent Posts Plugin for Quantox', 'namespace' ),
+
+			// widget options
+			array ( 'description' => __( 'Widget that displays recent posts', 'namespace' ) ) );
+        
+	}
+}
+
+// register Widget Recent Posts Plugin for Quantox
+function register_widget_recent_posts() {
+    register_widget( 'Widget_Recent_Posts_Plugin' );
+}
+add_action( 'widgets_init', 'register_widget_recent_posts' );

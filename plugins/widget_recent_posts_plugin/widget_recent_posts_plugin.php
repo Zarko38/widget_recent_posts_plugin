a39d7001aecd09ce829a7181138f4a91077e4f83
rnJambre <zarko_stn@yahoo.com>
2018-12-08 12:46:14 +0100

    first plugin commit

---
+++ plugins/widget_recent_posts_plugin/widget_recent_posts_plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Widget Recent Posts Plugin for Quantox
+Description: Custom recent posts for widget area plugin
+Author: Zarko Stojadinovic
+Version: 1.0.0
+Author URI: http://www.arcs.org.rs
+*/
+
+
